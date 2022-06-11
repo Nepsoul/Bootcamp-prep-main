15b4fbda9856827256c9f8b352b3be8c005562a0
Niru Magar <nirumagar901@gmail.com>
2022-06-11 11:07:00 +0545

    object method

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,28 @@
 // YOUR CODE BELOW
+
+//callThemAll: function (){}
+debugger;
+function callThemAll(object, Val) {
+  let result = [];
+  for (let key in object) {
+    let currVal = object[key];
+    if (typeof currVal === "function") {
+      result.push(currVal(Val));
+    }
+  }
+  return result;
+}
+
+let addsNums = {
+  addTen: function (num) {
+    return num + 10;
+  },
+
+  addTwenty: function (num) {
+    return num + 20;
+  },
+
+  someProperty: "value",
+};
+
+console.log(callThemAll(addsNums, 100)); // => [110, 120]
