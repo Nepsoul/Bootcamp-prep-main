7696573a086776e1dbfb830a64d5614bce7e8f47
Niru Magar <nirumagar901@gmail.com>
2022-06-13 15:14:57 +0545

    way to clone

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -2,9 +2,11 @@
 //myReverse([1, 2, 3]);    // => [3, 2, 1]
 function myReverse(array) {
   let result = [];
-  for (let i = array.length - 1; i >= 0; i--) {
+  for (i = array.length - 1; i >= 0; i--) {
     let currVal = array[i];
-    result.push(array[i]);
+    result.push(currVal);
   }
   return result;
 }
+let arr = myReverse([1, 2, 3]);
+console.log(arr);
--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+//unShift([1, 2, 3], 0); / => [0, 1, 2, 3];
+function myUnshift(array, value) {
+  let result = [value];
+  for (i = 0; i < array.length; i++) {
+    let currVal = array[i];
+    result.push(currVal);
+  }
+  return result;
+}
