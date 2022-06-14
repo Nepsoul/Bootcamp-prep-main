3718af4e529d47c94795296b8124024cb9e1695f
Niru Magar <nirumagar901@gmail.com>
2022-06-14 09:46:34 +0545

    my reverse

--- workshops/09-pbv-pbr/04-my-splice/my-splice.js
+++ workshops/09-pbv-pbr/04-my-splice/my-splice.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+// let myArray = [1, 2, 3];
+// mySplice(myArray, 1, 1, 'apples')) // => [2]
+// console.log(myArray)    // [1,'apples', 3]
+function mySplice(array) {
+  let result = [];
+
+  return result;
+}
--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function reverseArray(myArray) {
+  let result = [];
+  for (let i = myArray.length - 1; i >= 0; i--) {
+    let currVal = myArray[i];
+    result.push(myArray[i]);
+  }
+  for (let i = 0; i < result.length; i++) {
+    myArray[i] = result[i];
+  }
+  return result;
+}
