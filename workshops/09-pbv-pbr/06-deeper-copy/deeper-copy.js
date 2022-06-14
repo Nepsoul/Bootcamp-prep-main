296df05b7650ada52123991187c798ee45293e42
Niru Magar <nirumagar901@gmail.com>
2022-06-14 10:36:06 +0545

    deepercopy

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+// let myArray = [1, [2, 3]];
+// let copy = deeperCopy(myArray);
+// copy[1].push(4);
+// console.log(myArray); // [1, [2, 3]]
+// let copy2 = myArray.slice();
+// copy2[1].push(4);
+// console.log(myArray); // [1, [2, 3, 4]]
+
+function deeperCopy(array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let currVal = array[i];
+    if (Array.isArray(currVal)) {
+      let myArray = [];
+      for (let j = 0; j < currVal.length; j++) {
+        myArray.push(currVal);
+      }
+      result.push(myArray);
+    } else {
+      result.push(currVal);
+    }
+  }
+  return result;
+}
